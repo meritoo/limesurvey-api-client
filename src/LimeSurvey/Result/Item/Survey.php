472807ee33e20da3d6084cdda43100951b9997fa
Meritoo <krzysztof.niziol@meritoo.pl>
2017-10-22 18:42:07 +0200

    Participant & Survey - update creating instance od DateTime from string

--- src/LimeSurvey/Result/Item/Survey.php
+++ src/LimeSurvey/Result/Item/Survey.php
@@ -76,7 +76,7 @@ class Survey extends BaseItem
                     break;
                 }
 
-                $this->startsAt = Date::getDateTime($value, false, 'Y-m-d H:i:s');
+                $this->startsAt = Date::getDateTime($value, true);
                 break;
 
             case 'expires':
@@ -84,7 +84,7 @@ class Survey extends BaseItem
                     break;
                 }
 
-                $this->expiresAt = Date::getDateTime($value, false, 'Y-m-d H:i:s');
+                $this->expiresAt = Date::getDateTime($value, true);
                 break;
 
             case 'active':
